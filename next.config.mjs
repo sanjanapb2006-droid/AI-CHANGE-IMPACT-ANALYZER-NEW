d971180b3e44e03de7c2b840737863dcbc37db7d
Sanajana P B <sanjanapb2006@gmail.com>
2026-08-08 19:20:53 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
